61be76ee7d3a6a93d9e8c8d4081437d4cb0abb63
Ivan Y <ivany@subetuweb.site>
2022-04-28 23:23:08 -0500

    Refactoring after eslint fix

--- test/services/FizzbuzService.test.js
+++ test/services/FizzbuzService.test.js
@@ -1,50 +1,29 @@
-const FizzbuzzService = require("../../lib/services/FizzbuzzService");
+const FizzbuzzService = require("../../lib/services/FizzbuzService");
 
-describe("Test for ExplorerService", () => {
+describe("Testing FizzbuzService", () => {
 
-  test("1. Obtain trick number", () => {
-    const explorer1 = { name: "Explorer1", score: 1 };
-    const explorerResult = FizzbuzzService.applyValidationInExplorer(explorer1);
-    expect(explorerResult.trick).toBe(1);
-  });
+    test("Trick number retun score", () => {
+        const explorer1 = { name: "Explorer1", score: 1 };
+        const explorerResult = FizzbuzzService.applyValidationInExplorer(explorer1);
+        expect(explorerResult.trick).toBe(1);
+    });
 
-  test("2. Obtain trick fizz", () => {
-    const explorer3 = { name: "Explorer3", score: 3 };
-    const explorerResult = FizzbuzzService.applyValidationInExplorer(explorer3);
-    expect(explorerResult.trick).toBe("FIZZ");
-  });
+    test("Return Fizz", () => {
+        const explorer3 = { name: "Explorer3", score: 3 };
+        const explorerResult = FizzbuzzService.applyValidationInExplorer(explorer3);
+        expect(explorerResult.trick).toBe("FIZZ");
+    });
 
-  test("3. Obtain trick buzz", () => {
-    const explorer5 = { name: "Explorer5", score: 5 };
-    const explorerResult = FizzbuzzService.applyValidationInExplorer(explorer5);
-    expect(explorerResult.trick).toBe("BUZZ");
-  });
+    test("Return Buzz", () => {
+        const explorer5 = { name: "Explorer5", score: 5 };
+        const explorerResult = FizzbuzzService.applyValidationInExplorer(explorer5);
+        expect(explorerResult.trick).toBe("BUZZ");
+    });
 
-  test("4. Obtain trick fizzbuzz", () => {
-    const explorer15 = { name: "Explorer15", score: 15 };
-    const explorerResult = FizzbuzzService.applyValidationInExplorer(explorer15);
-    expect(explorerResult.trick).toBe("FIZZBUZZ");
-  });
-
-
-  test("5. Validation number Fizzbuzz", () => {
-    const explorerResult = FizzbuzzService.applyValidationInNumber(15);
-    expect(explorerResult).toBe("FIZZBUZZ");
-  });
-
-  test("6. Validation number Fizz", () => {
-    const explorerResult = FizzbuzzService.applyValidationInNumber(3);
-    expect(explorerResult).toBe("FIZZ");
-  });
-
-  test("7. Validation number buzz", () => {
-    const explorerResult = FizzbuzzService.applyValidationInNumber(5);
-    expect(explorerResult).toBe("BUZZ");
-  });
-
-  test("8. Validation number", () => {
-    const explorerResult = FizzbuzzService.applyValidationInNumber(1);
-    expect(explorerResult).toBe(1);
-  });
+    test("Return fizzbuzz", () => {
+        const explorer15 = { name: "Explorer15", score: 15 };
+        const explorerResult = FizzbuzzService.applyValidationInExplorer(explorer15);
+        expect(explorerResult.trick).toBe("FIZZBUZZ");
+    });
 
 });
